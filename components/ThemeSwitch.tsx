795ec561677ee43d2ec9241699c7cbed0cebdc2d
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-10-27 17:05:54 +0300

    add dark mode functionality

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,10 +1,45 @@
-import React from "react";
-import { BsSun } from "react-icons/bs";
+"use client";
+
+import { useEffect, useState } from "react";
+import { BsMoon, BsSun } from "react-icons/bs";
+
+type ITheme = "light" | "dark";
 
 export default function ThemeSwitch() {
+  const [theme, setTheme] = useState<ITheme>("light");
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      window.localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setTheme("light");
+      window.localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme") as ITheme | null;
+
+    if (localTheme) {
+      setTheme(localTheme);
+      if (localTheme === "dark") {
+        document.documentElement.classList.add("dark");
+      }
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme("dark");
+      document.documentElement.classList.add("dark");
+    }
+  }, []);
+
   return (
-    <button className="fixed bottom-5 right-5">
-      <BsSun />
+    <button
+      className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] borderBlack shadow-2xl rounded-full flex justify-center items-center hover:scale-[1.15] active:scale-105 transition-all"
+      onClick={toggleTheme}
+    >
+      {theme === "light" ? <BsSun /> : <BsMoon />}
     </button>
   );
 }
